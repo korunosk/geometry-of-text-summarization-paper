69196373c4af26453c85907edb639eff336ea2c3
korunosk <mladen.korunoski@epfl.ch>
2020-12-17 10:49:51 +0100

    Update configs

--- src/config_models.py
+++ src/config_models.py
@@ -1,58 +1,95 @@
-CONFIG_MODELS = {
-    'NNRougeRegModel': {
-        'D': 768,
-        'p': 2,
-        'blur': 0.05,
-        'scaling': 0.9,
-        'learning_rate': 0.01,
-        'batch_size': 128,
-        'epochs': 5
-    },
-    'NNWAvgPRModel': {
-        'D': 768,
-        'H': 1536,
-        'scaling_factor': 1,
-        'learning_rate': 0.01,
-        'batch_size': 128,
-        'epochs': 5
-    },
-    'LinSinkhornRegModel': {
-        'D': 768,
-        'p': 2,
-        'blur': 0.05,
-        'scaling': 0.9,
-        'learning_rate': 0.01,
-        'batch_size': 128,
-        'epochs': 5
-    },
-    'LinSinkhornPRModel': {
-        'D': 768,
-        'p': 2,
-        'blur': 0.05,
-        'scaling': 0.9,
-        'scaling_factor': 1,
-        'learning_rate': 0.01,
-        'batch_size': 128,
-        'epochs': 5
-    },
-    'NNSinkhornPRModel': {
-        'D': 768,
-        'p': 2,
-        'blur': 0.05,
-        'scaling': 0.9,
-        'scaling_factor': 1,
-        'learning_rate': 0.01,
-        'batch_size': 128,
-        'epochs': 5
-    },
-    'CondLinSinkhornPRModel': {
-        'D': 768,
-        'p': 2,
-        'blur': 0.05,
-        'scaling': 0.9,
-        'scaling_factor': 1,
-        'learning_rate': 0.01,
-        'batch_size': 128,
-        'epochs': 5
-    },
-}
+from src.config import EMBEDDING_METHODS
+
+
+def make_config_models(embedding_method, dataset_id):
+    PARAMETERS = {
+        EMBEDDING_METHODS[0]: {
+            'EMBEDDING_SIZE': 300,
+            'BATCH_SIZE': 128,
+            'EPOCHS': 5
+        }, # LSA
+        EMBEDDING_METHODS[1]: {
+            'EMBEDDING_SIZE': 300,
+            'BATCH_SIZE': 128,
+            'EPOCHS': 5
+        }, # GloVe
+        EMBEDDING_METHODS[2]: {
+            'EMBEDDING_SIZE': 300,
+            'BATCH_SIZE': 128,
+            'EPOCHS': 5
+        }, # fasttext
+        EMBEDDING_METHODS[3]: {
+            'EMBEDDING_SIZE': 768,
+            'BATCH_SIZE': 32 if dataset_id == 2 else 4,
+            'EPOCHS': 5
+        }, # BERT_word
+        EMBEDDING_METHODS[4]: {
+            'EMBEDDING_SIZE': 768,
+            'BATCH_SIZE': 128,
+            'EPOCHS': 5
+        }, # BERT_sent
+        EMBEDDING_METHODS[5]: {
+            'EMBEDDING_SIZE': 1024,
+            'BATCH_SIZE': 32 if dataset_id == 2 else 4,
+            'EPOCHS': 5
+        }, # BART_word
+    }
+
+    return {
+        'NNRougeRegModel': {
+            'D': PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'p': 2,
+            'blur': 0.05,
+            'scaling': 0.9,
+            'learning_rate': 0.01,
+            'batch_size': PARAMETERS[embedding_method]['BATCH_SIZE'],
+            'epochs': PARAMETERS[embedding_method]['EPOCHS']
+        },
+        'NNWAvgPRModel': {
+            'D': PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'H': 2 * PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'scaling_factor': 1,
+            'learning_rate': 0.01,
+            'batch_size': PARAMETERS[embedding_method]['BATCH_SIZE'],
+            'epochs': PARAMETERS[embedding_method]['EPOCHS']
+        },
+        'LinSinkhornRegModel': {
+            'D': PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'p': 2,
+            'blur': 0.05,
+            'scaling': 0.9,
+            'learning_rate': 0.01,
+            'batch_size': PARAMETERS[embedding_method]['BATCH_SIZE'],
+            'epochs': PARAMETERS[embedding_method]['EPOCHS']
+        },
+        'LinSinkhornPRModel': {
+            'D': PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'p': 2,
+            'blur': 0.05,
+            'scaling': 0.9,
+            'scaling_factor': 1,
+            'learning_rate': 0.01,
+            'batch_size': PARAMETERS[embedding_method]['BATCH_SIZE'],
+            'epochs': PARAMETERS[embedding_method]['EPOCHS']
+        },
+        'NNSinkhornPRModel': {
+            'D': PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'p': 2,
+            'blur': 0.05,
+            'scaling': 0.9,
+            'scaling_factor': 1,
+            'learning_rate': 0.01,
+            'batch_size': PARAMETERS[embedding_method]['BATCH_SIZE'],
+            'epochs': PARAMETERS[embedding_method]['EPOCHS']
+        },
+        'CondLinSinkhornPRModel': {
+            'D': PARAMETERS[embedding_method]['EMBEDDING_SIZE'],
+            'p': 2,
+            'blur': 0.05,
+            'scaling': 0.9,
+            'scaling_factor': 1,
+            'learning_rate': 0.01,
+            'batch_size': PARAMETERS[embedding_method]['BATCH_SIZE'],
+            'epochs': PARAMETERS[embedding_method]['EPOCHS']
+        },
+    }
--- src/config_transformers.py
+++ src/config_transformers.py
@@ -1,30 +1,74 @@
 import torch
-from src.implementation.transform.datasets import *
+from src.config import EMBEDDING_METHODS
+from src.implementation.transform.datasets import (
+    repeat_mean,
+    pad,
+    pad_h
+)
 
 
-TRANSFORMERS = {
-    'NNRougeRegModel': {
-        'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], [torch.tensor(document_embs, dtype=torch.float), None])),
-        'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], [None, None]))
-    },
-    'NNWAvgPRModel': {
-        'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, repeat_mean(document_embs, 15)))),
-        'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad(summary_embs, 15))))
-    },
-    'LinSinkhornRegModel': {
-        'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, 650)))),
-        'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, 15))))
-    },
-    'LinSinkhornPRModel': {
-        'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, 650)))),
-        'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, 15))))
-    },
-    'NNSinkhornPRModel': {
-        'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, 650)))),
-        'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, 15))))
-    },
-    'CondLinSinkhornPRModel': {
-        'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, 650)))),
-        'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, 15))))
+def make_config_transformers(embedding_method, dataset_id, exec):
+    PARAMETERS = {
+        EMBEDDING_METHODS[0]: {
+            'PADDING_DOCUMENTS': None,
+            'PADDING_SUMMARY': None
+        }, # LSA
+        EMBEDDING_METHODS[1]: {
+            'PADDING_DOCUMENTS': None,
+            'PADDING_SUMMARY': None
+        }, # GloVe
+        EMBEDDING_METHODS[2]: {
+            'PADDING_DOCUMENTS': None,
+            'PADDING_SUMMARY': None
+        }, # fasttext
+        EMBEDDING_METHODS[3]: {
+            'PADDING_DOCUMENTS': 1200 if dataset_id == 2 else 8850,
+            'PADDING_SUMMARY': 110 if dataset_id == 2 else 130
+        }, # BERT_word
+        EMBEDDING_METHODS[4]: {
+            'PADDING_DOCUMENTS': 650,
+            'PADDING_SUMMARY': 15
+        }, # BERT_sent
+        EMBEDDING_METHODS[5]: {
+            'PADDING_DOCUMENTS': 1200 if dataset_id == 2 else 8850,
+            'PADDING_SUMMARY': 110 if dataset_id == 2 else 130
+        }, # BART_WORD
     }
-}
+
+    TRANSFORMERS = {
+        'NNRougeRegModel': {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], [torch.tensor(document_embs, dtype=torch.float), None])),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], [None, None]))
+        },
+        'NNWAvgPRModel': {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, repeat_mean(document_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY'])))),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad(summary_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY']))))
+        },
+        'LinSinkhornRegModel': {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, PARAMETERS[embedding_method]['PADDING_DOCUMENTS'])))),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY']))))
+        },
+        'LinSinkhornPRModel': {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, PARAMETERS[embedding_method]['PADDING_DOCUMENTS'])))),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY']))))
+        },
+        'NNSinkhornPRModel': {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, PARAMETERS[embedding_method]['PADDING_DOCUMENTS'])))),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY']))))
+        },
+        'CondLinSinkhornPRModel': {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, PARAMETERS[embedding_method]['PADDING_DOCUMENTS'])))),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY']))))
+        }
+    }
+
+    # For NNRougeRegModel transformers, to get the
+    # correlation with human judgements, we use again
+    # the Sinkhorn loss.
+    if exec:
+        TRANSFORMERS['NNRougeRegModel'] = {
+            'transform_documents': lambda document_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(document_embs, PARAMETERS[embedding_method]['PADDING_DOCUMENTS'])))),
+            'transform_summary': lambda summary_embs: dict(zip(['embs', 'aux'], map(torch.from_numpy, pad_h(summary_embs, PARAMETERS[embedding_method]['PADDING_SUMMARY']))))
+        }
+    
+    return TRANSFORMERS
